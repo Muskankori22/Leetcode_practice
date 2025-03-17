46653ebb091a1f703c44bb33e673a86ccd09d422
Muskan Kori <muskankori024@gmail.com>
2025-03-17 19:02:02 +0530

    Time: 1 ms (71.09%), Space: 41.9 MB (90.94%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,49 +1,42 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-       List<Integer> ans = new ArrayList<>();
-       Queue<TreeNode> qu = new LinkedList<>();
-       if(root==null)
-       {
-        return ans;
-       }
-       qu.offer(root); 
-       while(!qu.isEmpty())
-       {
-        int size = qu.size();
-        while(size>0)
-        {
-            TreeNode node = qu.poll();
-             if(node.left!=null)
-            {
-                qu.offer(node.left);
-            }
-            if(node.right!=null)
-            {
-                qu.offer(node.right);
-            }
-            
-            size--;
-            if(size==0)
-            {
-                ans.add(node.val);
+        List<Integer> ans = new ArrayList<>();
+        Queue<TreeNode> qu = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        qu.offer(root);
+        while (!qu.isEmpty()) {
+            int size = qu.size();
+            while (size > 0) {
+                TreeNode node = qu.poll();
+                if (node.left != null) {
+                    qu.offer(node.left);
+                }
+                if (node.right != null) {
+                    qu.offer(node.right);
+                }
+                size--;
+                if (size == 0) {
+                    ans.add(node.val);
+                }
             }
         }
-       }
-       return ans;
+        return ans;
     }
 }
