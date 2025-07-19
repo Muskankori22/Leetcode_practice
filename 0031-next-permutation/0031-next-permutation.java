f930bf110b96c7122e8e567b1bb54ed80a6f1b87
Muskan Kori <muskankori024@gmail.com>
2025-07-19 11:19:11 +0530

    Time: 1 ms (14.04%), Space: 43.3 MB (36.41%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,36 +1,44 @@
+import java.util.Arrays;
+
 class Solution {
-    public static void reverse(int num[],int start)
-    {
-        int i = start;
-        int j = num.length-1;
-        while(i<j)
-        {
-            swap(num,i,j);
-            i++;
-            j--;
-        }
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-    public static void swap(int num[],int i,int j)
-    {
-        int temp = num[i];
-        num[i] = num[j];
-        num[j] = temp;
-    }
+
     public void nextPermutation(int[] nums) {
-        int i = nums.length-2;
-        while(i>=0 && nums[i+1]<=nums[i])
-        {
-            i--;
-        }
-        if(i>=0)
-        {
-            int j = nums.length-1;
-            while(nums[j] <= nums[i])
-            {
-                j--;
+        int pivot = -1;
+
+        // Step 1: Find the pivot
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
             }
-            swap(nums,i,j);
         }
-        reverse(nums,i+1);
+
+        // Step 2: If no pivot, just reverse the whole array
+        if (pivot == -1) {
+            Arrays.sort(nums);
+            return;
+        }
+
+        // Step 3: Find the rightmost element greater than pivot and swap
+        for (int i = nums.length - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                swap(nums, i, pivot);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the suffix starting after pivot
+        int start = pivot + 1;
+        int end = nums.length - 1;
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
     }
 }
