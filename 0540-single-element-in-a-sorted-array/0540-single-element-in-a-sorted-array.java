fdde72beebcc1775545f87a7aa58193fec89c008
Muskan Kori <muskankori024@gmail.com>
2025-08-16 12:20:04 +0530

    Time: 0 ms (100%), Space: 50.3 MB (65.87%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,24 +1,42 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-       int ans = -1;
-       HashMap<Integer,Integer> hs = new HashMap<>();
-       for(int i=0;i<nums.length;i++)
-       {
-        if(hs.containsKey(nums[i]))
-        {
-            hs.put(nums[i],hs.get(nums[i])+1);
+        int s = 0;
+        int e = nums.length - 1;
+        int n = nums.length;
+
+        if (n == 1) return nums[0];
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+
+            // Check if mid is the answer
+            if ((mid == 0 || nums[mid] != nums[mid - 1]) &&
+                (mid == n - 1 || nums[mid] != nums[mid + 1])) {
+                return nums[mid];
+            }
+
+            // If mid is even
+            if (mid % 2 == 0) {
+                if (nums[mid] == nums[mid + 1]) {
+                    // pair is fine, go right
+                    s = mid + 2;
+                } else {
+                    // mismatch, single is on the left
+                    e = mid - 1;
+                }
+            } 
+            // If mid is odd
+            else {
+                if (nums[mid] == nums[mid - 1]) {
+                    // pair is fine, go right
+                    s = mid + 1;
+                } else {
+                    // mismatch, single is on the left
+                    e = mid - 1;
+                }
+            }
         }
-        else{
-            hs.put(nums[i],1);
-        }
-       } 
-       for(int i=0;i<nums.length;i++)
-       {
-        if(hs.get(nums[i])==1)
-        {
-            ans = nums[i];
-        }
-       }
-       return ans;
+
+        return -1; // should never reach here
     }
 }
